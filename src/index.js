1dd9c21d99f17eb308070bd4b4b3f6c0ba7448d6
Yifei Yin <yifeiyin@foxmail.com>
2020-02-11 17:35:48 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,10 +11,11 @@ import {
   ScrollView,
   Dimensions,
   TouchableOpacity,
-  ViewPagerAndroid,
   Platform,
   ActivityIndicator
 } from 'react-native'
+
+import ViewPager from 'react-native-community/react-native-viewpager'
 
 /**
  * Default styles
@@ -649,7 +650,7 @@ export default class extends Component {
        )
     }
     return (
-      <ViewPagerAndroid ref={this.refScrollView}
+      <ViewPager ref={this.refScrollView}
         {...this.props}
         initialPage={this.props.loop ? this.state.index + 1 : this.state.index}
         onPageScrollStateChanged={this.onPageScrollStateChanged}
@@ -657,7 +658,7 @@ export default class extends Component {
         key={pages.length}
         style={[styles.wrapperAndroid, this.props.style]}>
         {pages}
-      </ViewPagerAndroid>
+      </ViewPager>
     )
   }
 
